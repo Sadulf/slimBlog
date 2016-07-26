b3145e345521322c6aee998761e0e37b077a6697
Sadulf <sandulf@mail.ru>
2016-07-26 17:53:52 +0300

    Routes updated (all routes taken out from init.php to config.php)

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ $config = [
 
         'logger' => [
             'name' => 'app',
-            'path' => ROOT. '/logs/app.log',
-            'level'=> \Monolog\Logger::DEBUG
+            'path' => ROOT . '/logs/app.log',
+            'level' => \Monolog\Logger::DEBUG
         ],
 
         'db' => [
@@ -20,11 +20,97 @@ $config = [
         ],
 
         'twig' => [
-    		'settings' => [
-    			'debug' => true,
- //   			'cache' => ROOT.'/cache',
-    		],
-    		'template' => ROOT.'/template'
+            'settings' => [
+                'debug' => true,
+                //   			'cache' => ROOT.'/cache',
+            ],
+            'template' => ROOT . '/template'
         ],
     ],
+    'routes' => [
+        // admin routes
+        'AdminController:indexAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/',
+            'action'=>'\AdminController:indexAction',
+            'inMenu'=>'admin',
+            'menuTitle'=>'Админпанель'
+        ],
+        'AdminController:indexPageAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/index/',
+            'action'=>'\AdminController:indexPageAction',
+            'inMenu'=>'admin',
+            'menuTitle'=>'Главная'
+        ],
+        'AdminController:categoriesAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/categories/',
+            'action'=>'\AdminController:categoriesAction',
+            'inMenu'=>'admin',
+            'menuTitle'=>'Категории'
+        ],
+        'AdminController:categoryAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/category/{id}/',
+            'action'=>'\AdminController:categoryAction'
+        ],
+        'AdminController:articlesAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/articles/',
+            'action'=>'\AdminController:articlesAction',
+            'inMenu'=>'admin',
+            'menuTitle'=>'Публикации'
+        ],
+        'AdminController:articleAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/article/{id}/',
+            'action'=>'\AdminController:articleAction'
+        ],
+        'AdminController:staticAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/static/',
+            'action'=>'\AdminController:staticAction',
+            'inMenu'=>'admin',
+            'menuTitle'=>'Статичные страницы'
+        ],
+        'AdminController:staticPageAction'=> [
+            'method'=>'GET',
+            'path'=>'/admin/static_page/{id}/',
+            'action'=>'\AdminController:staticPageAction'
+        ],
+
+        'AdminController:indexPageSaveAction'=> [
+            'method'=>'POST',
+            'path'=>'/admin/index/edit/',
+            'action'=>'\AdminController:indexPageSaveAction'
+        ],
+        'AdminController:categorySaveAction'=> [
+            'method'=>'POST',
+            'path'=>'/admin/category/{id}/',
+            'action'=>'\AdminController:categorySaveAction'
+        ],
+        'AdminController:articleSaveAction'=> [
+            'method'=>'POST',
+            'path'=>'/admin/article/{id}/',
+            'action'=>'\AdminController:articleSaveAction'
+        ],
+
+        // blog routes
+        'MainController:categoryAction'=> [
+            'method'=>'GET',
+            'path'=>'/{id}/[page-{page}/]',
+            'action'=>'\MainController:categoryAction'
+        ],
+        'MainController:articleAction'=> [
+            'method'=>'GET',
+            'path'=>'/{id}.html',
+            'action'=>'\MainController:articleAction'
+        ],
+        'MainController:indexAction'=> [
+            'method'=>'GET',
+            'path'=>'/[page-{page}/]',
+            'action'=>'\MainController:indexAction'
+        ]
+    ]
 ];
--- engine/init.php
+++ engine/init.php
@@ -1,11 +1,12 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../config.php';
-require __DIR__.'/../engine/models/db.php';
-require __DIR__.'/../engine/models/blog.php';
-require __DIR__.'/../engine/MainController.php';
-require __DIR__.'/../engine/AdminController.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config.php';
+require __DIR__ . '/../engine/models/db.php';
+require __DIR__ . '/../engine/models/blog.php';
+require __DIR__ . '/../engine/models/admin.php';
+require __DIR__ . '/../engine/MainController.php';
+require __DIR__ . '/../engine/AdminController.php';
 
 
 // init slim
@@ -17,8 +18,8 @@ $container = $app->getContainer();
 // init db
 
 db::setConfig(
-    'mysql:host=' . $container['settings']['db']['host'] . ';dbname='  . $container['settings']['db']['name'] . ';charset=utf8',
-    $container['settings']['db']['user'], 
+    'mysql:host=' . $container['settings']['db']['host'] . ';dbname=' . $container['settings']['db']['name'] . ';charset=utf8',
+    $container['settings']['db']['user'],
     $container['settings']['db']['pass']
 );
 
@@ -39,7 +40,7 @@ $container['errorHandler'] = function ($c) {
 
 // add containers
 
-$container['logger'] = function($c) {
+$container['logger'] = function ($c) {
     // create a log channel
     $log = new \Monolog\Logger($c['settings']['logger']['name']);
     $log->pushHandler(new \Monolog\Handler\StreamHandler($c['settings']['logger']['path'], $c['settings']['logger']['level']));
@@ -56,36 +57,20 @@ $container['db'] = function () {
 // configure twig
 
 $twig_functions = [
-	'date'=> function ($arg) {
-    	return date('d.m.Y',$arg);
-	}
+    'date' => function ($arg) {
+        return date('d.m.Y', $arg);
+    }
 ];
 
 $container['twig'] = new Twig_Environment(
-	new Twig_Loader_Filesystem($container->get('settings')['twig']['template']),
-	$container->get('settings')['twig']['settings']
+    new Twig_Loader_Filesystem($container->get('settings')['twig']['template']),
+    $container->get('settings')['twig']['settings']
 );
 
-foreach($twig_functions as $name=>$func)
-    $container['twig']->addFunction(new Twig_SimpleFunction($name,$func));
+foreach ($twig_functions as $name => $func)
+    $container['twig']->addFunction(new Twig_SimpleFunction($name, $func));
 
 
-// add admin routes
-
-$app->get('/admin/', '\AdminController:indexAction');
-$app->get('/admin/index/', '\AdminController:indexPageAction');
-$app->get('/admin/categories/', '\AdminController:categoriesAction');
-$app->get('/admin/category/{id}/', '\AdminController:categoryAction');
-$app->get('/admin/articles/', '\AdminController:articlesAction');
-$app->get('/admin/article/{id}/', '\AdminController:articleAction');
-
-$app->post('/admin/index/edit/', '\AdminController:indexPageSaveAction');
-$app->post('/admin/category/{id}/', '\AdminController:categorySaveAction');
-$app->post('/admin/article/{id}/', '\AdminController:articleSaveAction');
-
-
-// add blog routes
-
-$app->get('/{id}/[page-{page}/]', '\MainController:categoryAction');
-$app->get('/{id}.html', '\MainController:articleAction');
-$app->get('/[page-{page}/]', '\MainController:indexAction');
+// add routes
+foreach($config['routes'] as $name=>$item)
+    $app->map([$item['method']],$item['path'],$item['action'])->setName($name);
